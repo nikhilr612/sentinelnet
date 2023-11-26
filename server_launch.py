83ed8e93aed4eae45d669b71cbb316b6ab1beb8c
lalith-AE30 <lalithae30@gmail.com>
2023-11-26 13:06:50 +0530

    Added redis database

--- server_launch.py
+++ server_launch.py
@@ -1,28 +1,41 @@
 import msgpack
 import numpy as np
 import redis
+from redis.commands.json.path import Path
 from server import Server
 from llog import msgupack_hook, LogType
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-if not r.exists('cpu_usage'):
-    r.rpush('cpu_usage')
-if not r.exists('mem_usage'):
-    r.rpush('mem_usage')
+
 
 def servlet_plot(conn):
+    # TODO Add a way to distinguish between devices when storing to DB
     reader = conn.makefile(mode='rb')
     data = msgpack.unpack(reader, ext_hook=msgupack_hook)
 
+    if not r.json().get('computer:1'):
+        r.json().set('computer:1', Path.root_path(), {})
+
     for log in data:
         if LogType(log[1]) == LogType.CPU_USAGE:
-            r.rpush('cpu_usage',np.array((log[0], log[2])))
+            data_point = (log[0], log[2])
+
+            if 'cpu_usage' in r.json().objkeys('computer:1'):
+                r.json().arrappend('computer:1', 'cpu_usage', data_point)
+            else:
+                r.json().set('computer:1', 'cpu_usage', [data_point])
+
         if LogType(log[1]) == LogType.MEM_USAGE:
-            mem_usage.append(np.array((log[0], log[2])))
+            data_point = (log[0], log[2])
+
+            if 'mem_usage' in r.json().objkeys('computer:1'):
+                r.json().arrappend('computer:1', 'mem_usage', data_point)
+            else:
+                r.json().set('computer:1', 'mem_usage', [data_point])
 
     reader.close()
     conn.close()
-    # TODO Connect to Redis or make respones transferrable to analysis and gui
+
 
 s = Server(target=servlet_plot)
 s.start()
