76adbf617eeb6bb005c22683210c386f6c451ff9
lalith-AE30 <lalithae30@gmail.com>
2023-11-27 12:37:12 +0530

    Add redis.conf, packet monitoring doesn't seem to appear in logs yet

--- server_launch.py
+++ server_launch.py
@@ -10,13 +10,14 @@ r = redis.Redis(host='localhost', port=6379, db=0)
 
 
 def action(conn):
-    # TODO Add a way to distinguish between devices when storing to DB
     reader = conn.makefile(mode='rb')
     data = msgpack.unpack(reader, ext_hook=msgupack_hook)
 
+    # TODO Add a way to distinguish between devices when storing to DB
     if not r.json().get('computer:1'):
         r.json().set('computer:1', Path.root_path(), {})
 
+    # TODO Refactor to uniformly store to database using enum metadata
     for log in data:
         match LogType(log[1]):
             case LogType.CPU_USAGE:
@@ -34,10 +35,33 @@ def action(conn):
                     r.json().arrappend('computer:1', 'mem_usage', data_point)
                 else:
                     r.json().set('computer:1', 'mem_usage', [data_point])
-            
+
+            case LogType.DISK_READ:
+                data_point = (log[0], *log[2])
+
+                if 'disk_read' in r.json().objkeys('computer:1'):
+                    r.json().arrappend('computer:1', 'disk_read', data_point)
+                else:
+                    r.json().set('computer:1', 'disk_read', [data_point])
+
+            case LogType.DISK_WRITE:
+                data_point = (log[0], *log[2])
+
+                if 'disk_write' in r.json().objkeys('computer:1'):
+                    r.json().arrappend('computer:1', 'disk_write', data_point)
+                else:
+                    r.json().set('computer:1', 'disk_write', [data_point])
+
+            case LogType.NEW_PROCESS:
+                data_point = (log[0], *log[2][:1])
+
+                if 'processes' in r.json().objkeys('computer:1'):
+                    r.json().arrappend('computer:1', 'processes', data_point)
+                else:
+                    r.json().set('computer:1', 'processes', [data_point])
+
             case other:
                 print(f"TYPE: {other}\nTIME: {log[0]}\nDATA: {log[2]}")
-
 
     reader.close()
     conn.close()
